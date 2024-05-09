680d2365f26a1748e10b7278b21e57a639e837d2
Jinnie <hjk7639@gmail.com>
2024-05-09 21:52:38 +0900

    050924 - Leetcode 2629 add if statement for early return

--- Leetcode/050924_2629.ts
+++ Leetcode/050924_2629.ts
@@ -1,6 +1,9 @@
 type F = (x: number) => number;
 
 function compose(functions: F[]): F {
+  if (functions.length === 0) {
+    return (x: number) => x;
+  }
   return function (x: number) {
     let answer = x;
     for (let i = functions.length - 1; i >= 0; i--) {
