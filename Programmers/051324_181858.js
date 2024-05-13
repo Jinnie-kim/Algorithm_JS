d3ad23cf694695a5585e8a538579397b08dbdfc4
Jinnie <hjk7639@gmail.com>
2024-05-13 22:07:40 +0900

    051324 - Programmers 181858

---
+++ Programmers/051324_181858.js
@@ -0,0 +1,16 @@
+function solution(arr, k) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (answer.length === k) return;
+
+    if (answer.indexOf(arr[i]) === -1) answer.push(arr[i]);
+  }
+  while (answer.length < k) {
+    answer.push(-1);
+  }
+  return answer;
+}
+
+solution([0, 1, 1, 2, 2, 3], 3);
+solution([0, 1, 1, 1, 1], 4);
