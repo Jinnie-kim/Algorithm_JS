4d0cf1f1a4aba4a51e874d9ff7b9e1a0d451a0f7
Jinnie <hjk7639@gmail.com>
2024-06-03 20:53:58 +0900

    060324 - Programmers 120812

---
+++ Programmers/060324_120812.js
@@ -0,0 +1,10 @@
+function solution(array) {
+  const map = new Map();
+
+  for (let n of array) {
+    map.set(n, (map.get(n) || 0) + 1);
+  }
+
+  map = [...map].sort((a, b) => b[1] - a[1]);
+  return map.length === 1 || map[0][1] > map[1][1] ? map[0][0] : -1;
+}
